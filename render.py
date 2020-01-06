704d3ba3fc4226ab35cafd6070d8b016a7eabc39
Sander van de Graaf <sander@vandegraaf.nl>
2020-01-06 08:59:15 +0100

    bump

--- render.py
+++ render.py
@@ -4,24 +4,24 @@ import mako.lookup
 import os
 
 # Build the template lookup.
-lookup = mako.lookup.TemplateLookup(["templates"],
-    preprocessor=haml.preprocessor
-)
+lookup = mako.lookup.TemplateLookup(
+    ["templates"], preprocessor=haml.preprocessor
+)  # noqa
 
 # parse all haml files which don't start with _
-for filename in os.listdir('./templates'):
-    if filename[0] != '_' and filename.split('.')[-1] == 'haml':
-        template_name = filename.split('.')[0]
+for filename in os.listdir("./templates"):
+    if filename[0] != "_" and filename.split(".")[-1] == "haml":
+        template_name = filename.split(".")[0]
 
         # Retrieve a template.
-        template = lookup.get_template('%s.haml' % template_name)
+        template = lookup.get_template("%s.haml" % template_name)
 
-        destination_file = './build/%s.html' % template_name
+        destination_file = "./build/%s.html" % template_name
 
         # Render!
         print("Rendering %s.haml to %s" % (template_name, destination_file))
-        with open(destination_file, 'w') as fh:
+        with open(destination_file, "w") as fh:
             try:
                 fh.write(template.render())
-            except:
+            except Exception:
                 print(mako.exceptions.text_error_template().render())
